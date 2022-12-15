9a7d5c9d7221dab0e491ae1744b38c8c570da162
Muhamed Zuberovic <muhamed.zuberovic@isatis.com>
2022-12-15 23:14:23 +0100

    Naming typo fixes

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="home">
 		<img alt="Vue logo" src="./images/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue3 Boilerplate" />
+		<HelloWorld msg="Welcome to Your Vue3 Boilerplate WebPack V2" />
 	</div>
 </template>
 
