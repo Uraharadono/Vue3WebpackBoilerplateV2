0b8cabd7846a50d9e373480763305dc04243b2c0
Muhamed Zuberovic <muhamed.zuberovic@isatis.com>
2022-12-16 09:19:10 +0100

    Toggle shared component logic

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -2,15 +2,23 @@
 	<div class="about">
 		<h1>This is an about page</h1>
 		<!--<ComponentLoader2 />-->
+		<Toggle v-model="isToggled" />
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 // import ComponentLoader2 from '@/views/shared/ComponentLoader2.vue';
+import Toggle from '@/views/shared/Toggle.vue';
 
 export default defineComponent({
 	name: 'AboutPage',
 	//components: { ComponentLoader2 },
+	components: { Toggle },
+	data() {
+		return {
+			isToggled: false,
+		};
+	},
 });
 </script>
---
+++ src/views/shared/Toggle.vue
@@ -0,0 +1,44 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- If this fails take a look at: https://dev.to/vaibhavkhulbe/create-a-custom-toggle-component-in-vue-js-1ipi -->
+
+<template>
+	<!-- This syntax below works, but it's with buttons, and it looks kind of gross, not gonna lie. -->
+	<!-- <div>
+    <button v-if="value" class="btn btn-outline-light btn-square" @click="toggle()">
+      <span class="btn-icon">
+        <font-awesome-icon icon="sort-down" />
+      </span>
+    </button>
+    <button v-else class="btn btn-outline-light btn-square" @click="toggle()">
+      <span class="btn-icon">
+        <font-awesome-icon icon="sort-up" />
+      </span>
+    </button>
+  </div> -->
+
+	<div>
+		<!--<font-awesome-icon v-if="toggled" icon="sort-down" @click="toggle()" />
+		<font-awesome-icon v-else icon="caret-right" @click="toggle()" />-->
+		<span v-if="toggleState" @click="toggle()"> ˄ </span>
+		<span v-else @click="toggle()"> ˅ </span>
+	</div>
+</template>
+
+<script>
+export default {
+	// props: ['value'],
+	props: {
+		toggled: { type: Boolean, default: false },
+	},
+	data() {
+		return { toggleState: this.toggled };
+	},
+	methods: {
+		toggle() {
+			this.toggleState = !this.toggleState;
+			// eslint-disable-next-line vue/require-explicit-emits
+			this.$emit('toggle', this.toggleState);
+		},
+	},
+};
+</script>
